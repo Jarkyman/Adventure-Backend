62a59cb24cd2432cde042f24d187c4fe395a627c
Jarkyman <Jarkyman@users.noreply.github.com>
2022-03-13 10:34:23 +0000

    Google Java Format

--- src/main/java/com/example/adventurebackend/AdventureBackendApplication.java
+++ src/main/java/com/example/adventurebackend/AdventureBackendApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AdventureBackendApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(AdventureBackendApplication.class, args);
-    }
-
+  public static void main(String[] args) {
+    SpringApplication.run(AdventureBackendApplication.class, args);
+  }
 }
