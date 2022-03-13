f7df4a8a167a9c9e5b0efb21daf73a6393d14a91
jackiejensen <Jackiehartvig@hotmail.com>
2022-03-13 12:09:59 +0100

    Added project

---
+++ src/main/java/com/example/adventurebackend/repository/AdventuresRepository.java
@@ -0,0 +1,7 @@
+package com.example.adventurebackend.repository;
+
+import com.example.adventurebackend.model.Adventure;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdventuresRepository extends JpaRepository<Adventure, String> {
+}
