b4085aae5f3089aed3781beda80291784536e912
Christoffer <christofferp97@hotmail.com>
2022-03-14 13:58:05 +0100

    TX-5: added employee model, controller and repos

--- src/main/java/com/example/adventurebackend/config/InitData.java
+++ src/main/java/com/example/adventurebackend/config/InitData.java
@@ -1,12 +1,16 @@
 package com.example.adventurebackend.config;
 
 import com.example.adventurebackend.repository.AdventuresRepository;
+import com.example.adventurebackend.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 
 public class InitData implements CommandLineRunner {
 
   @Autowired AdventuresRepository adventuresRepository;
+
+  @Autowired
+  EmployeeRepository employeeRepository;
 
   @Override
   public void run(String... args) throws Exception {}
---
+++ src/main/java/com/example/adventurebackend/repository/EmployeeRepository.java
@@ -0,0 +1,8 @@
+package com.example.adventurebackend.repository;
+
+import com.example.adventurebackend.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, String> {
+
+}
