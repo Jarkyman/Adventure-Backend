3a7ce812f97fb67c527db2adfd48ff67e423870d
Christoffer <christofferp97@hotmail.com>
2022-03-15 09:54:01 +0100

    Clean up in unused classes

--- src/main/java/com/example/adventurebackend/config/InitData.java
+++ src/main/java/com/example/adventurebackend/config/InitData.java
@@ -1,7 +1,6 @@
 package com.example.adventurebackend.config;
 
 import com.example.adventurebackend.repository.ActivityRepository;
-import com.example.adventurebackend.repository.AdventuresRepository;
 import com.example.adventurebackend.repository.BookingRepository;
 import com.example.adventurebackend.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +8,6 @@ import org.springframework.boot.CommandLineRunner;
 
 public class InitData implements CommandLineRunner {
 
-  @Autowired AdventuresRepository adventuresRepository;
 
   @Autowired EmployeeRepository employeeRepository;
 
