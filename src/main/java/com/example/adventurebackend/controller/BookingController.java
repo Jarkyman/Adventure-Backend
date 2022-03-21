c5e589d6246bbaeaa362aa64a6bd673906aea8ee
jackiejensen <Jackiehartvig@hotmail.com>
2022-03-21 10:51:25 +0100

    added error msg

--- src/main/java/com/example/adventurebackend/controller/BookingController.java
+++ src/main/java/com/example/adventurebackend/controller/BookingController.java
@@ -73,7 +73,7 @@ public class BookingController {
       bookingRepository.deleteById(id);
       return new ResponseEntity<>("deleted booking by id = " + id, HttpStatus.OK);
     } catch (Exception err) {
-      return new ResponseEntity<>("can't delete booking by id = " + id, HttpStatus.NOT_FOUND);
+      return new ResponseEntity<>("can't delete booking by id = " + id + "\n ERROR: " + err, HttpStatus.NOT_FOUND);
     }
   }
 }
