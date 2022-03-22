3cd2e6c6ecaf98858b01c30c1ecaa100019006a1
Dubz97 <Dubz97@users.noreply.github.com>
2022-03-22 09:49:09 +0000

    Google Java Format

--- src/main/java/com/example/adventurebackend/controller/EmployeeController.java
+++ src/main/java/com/example/adventurebackend/controller/EmployeeController.java
@@ -14,8 +14,7 @@ import java.util.Optional;
 @CrossOrigin
 public class EmployeeController {
 
-  @Autowired
-  EmployeeRepository employeeRepository;
+  @Autowired EmployeeRepository employeeRepository;
 
   /**
    * List of employees
